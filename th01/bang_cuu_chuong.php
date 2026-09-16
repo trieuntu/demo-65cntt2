c31251776a57d6354f41e0d23487512ed7cf11cd
Trieu Nguyen Hai <trieu.science@Trieus-MacBook-Pro.local>
2026-09-16 14:22:38 +0700

    first ex

--- th01/bang_cuu_chuong.php
+++ th01/bang_cuu_chuong.php
@@ -6,6 +6,17 @@
     <title>Bang cuu chuong</title>
 </head>
 <body>
-    
+    <?php
+    echo "<h1>Bảng cửu chương</h1>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<h2>Bảng cửu chương $i</h2>";
+        echo "<ul>";
+        for ($j = 1; $j <= 10; $j++) {
+            $result = $i * $j;
+            echo "<li>$i x $j = $result</li>";
+        }
+        echo "</ul>";
+    }
+    ?>
 </body>
 </html>
